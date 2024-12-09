f52f049ac1ffd03059731bbadc10da2d190bbb80
teacher-mgt <min-k-t@hanmail.net>
2024-12-09 16:03:50 +0900

    20241209

--- app07/src/main/java/com/min/app07/dao/BoardDaoImpl.java
+++ app07/src/main/java/com/min/app07/dao/BoardDaoImpl.java
@@ -3,7 +3,7 @@ package com.min.app07.dao;
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.min.app07.dto.BoardDto;
@@ -12,55 +12,66 @@ import com.min.app07.dto.BoardDto;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@Repository
+@Repository  // DAO에서 사용하는 @Component입니다. Spring Container에 IBoardDao 타입의 bean이 생성됩니다.
 public class BoardDaoImpl implements IBoardDao {
 
   private final SqlSessionTemplate template;
   
   /* 
-  ** ************************ **
-  ** @RequiredArgsConstructor **
-  ** ************************ **
-  public BoardDaoImpl(@NonNull SqlSessionTemplate template) {
-    super();
-    this.template = template;
-  }
+   ** ************************ **
+   ** @RequiredArgsConstructor **
+   ** ************************ **
+    @Autowired
+    public BoardDaoImpl(@NonNull SqlSessionTemplate template) {
+      super();
+      this.template = template;
+    }
   */
 
+  /*
+   * 매퍼의 SQL 구문 호출하는 방식
+   * 
+   * selectList(SQL's id[, Parameter])
+   *  selectOne(SQL's id[, Parameter])
+   *     insert(SQL's id[, Parameter])
+   *     update(SQL's id[, Parameter])
+   *     delete(SQL's id[, Parameter])
+   */
+  
   @Override
   public List<BoardDto> selectBoardList() {
-    // TODO Auto-generated method stub
-    return null;
+    List<BoardDto> boardList = template.selectList("mybatis.mappers.boardMapper.selectBoardList");
+    return boardList;
   }
 
   @Override
   public int selectBoardCount() {
-    // TODO Auto-generated method stub
-    return 0;
+    int boardCount = template.selectOne("mybatis.mappers.boardMapper.selectBoardCount");
+    return boardCount;
   }
 
   @Override
   public BoardDto selectBoardById(int boardId) {
-    // TODO Auto-generated method stub
-    return null;
+    BoardDto boardDto = template.selectOne("mybatis.mappers.boardMapper.selectBoardById", boardId);
+    return boardDto;
   }
 
   @Override
   public int insertBoard(BoardDto boardDto) {
-    // TODO Auto-generated method stub
-    return 0;
+    int result = template.insert("mybatis.mappers.boardMapper.insertBoard", boardDto);
+    return result;
   }
 
   @Override
   public int updateBoard(BoardDto boardDto) {
-    // TODO Auto-generated method stub
-    return 0;
+    int result = template.update("mybatis.mappers.boardMapper.updateBoard", boardDto);
+    return result;
   }
 
   @Override
   public int deleteBoard(int boardId) {
-    // TODO Auto-generated method stub
-    return 0;
+    int result = template.delete("mybatis.mappers.boardMapper.deleteBoard", template);
+    return result;
   }
 
 }
